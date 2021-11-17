7694481ab6032339ef019fc31a568d6ff5275d6a
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2021-11-17 16:50:31 +0100

    kommit en bra bit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hej hej");
+const minKnapp = document.getElementById("minKnapp");
+
+const litetNamn = document.getElementById("litetNamn")
+
+minKnapp.addEventListener("click", function(){
+
+    let dittStoraNamn = litetNamn.value.toUpperCase();
+
+    console.log(dittStoraNamn);
+
+    litetNamn.append("Nu funkar det");
+
+
+});
